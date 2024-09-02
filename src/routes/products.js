1b796ffe54c08eca45c2b7a09585050e2eb55902
MpitasiWeb <marco_pitassi@hotmail.com>
2024-09-02 19:39:46 -0300

    Iniciamos el proyecto con Git

---
+++ src/routes/products.js
@@ -0,0 +1,98 @@
+import { Router } from "express";
+import { products } from "../../prod.js";
+
+const router = Router();
+
+router.get("/", (req, res) => {
+  res.send(products);
+});
+
+router.get("/:pid", (req, res) => {
+  const idProduct = +req.params.pid;
+  const product = products.find((product) => product.id === idProduct);
+  if (!product) {
+    res.status(404).send({ status: "error", error: "Producto no encontrado" });
+  } else {
+    res.send(product);
+  }
+});
+
+router.post("/", (req, res) => {
+  let newProduct = req.body;
+
+  newProduct = { ...newProduct, id: products.length + 1 };
+
+  if (!newProduct.thumbnails) {
+    newProduct = { ...newProduct, thumbnails: [] };
+  }
+
+  if (!newProduct.status) {
+    newProduct = { ...newProduct, status: true };
+  }
+
+  if (
+    !newProduct.title ||
+    !newProduct.description ||
+    !newProduct.code ||
+    !newProduct.price ||
+    !newProduct.status ||
+    !newProduct.stock
+  ) {
+    return res
+      .status(422)
+      .send({ status: "error", error: "valores incompletos" });
+  }
+
+  products.push(newProduct);
+  res.send({ status: "success", message: "producto añadido correctamente" });
+});
+
+router.put("/:id", (req, res) => {
+  const idProduct = +req.params.id;
+  let updateProduct = req.body;
+
+  const i = products.findIndex((product) => product.id === idProduct);
+
+  if (i === -1) {
+    return res
+      .status(404)
+      .send({ status: "error", error: "Producto no encontrado" });
+  }
+
+  if (!updateProduct.thumbnails) {
+    updateProduct = { ...updateProduct, thumbnails: [] };
+  }
+
+  if (
+    !updateProduct.title ||
+    !updateProduct.description ||
+    !updateProduct.code ||
+    !updateProduct.price ||
+    !updateProduct.status ||
+    !updateProduct.stock
+  ) {
+    return res
+      .status(422)
+      .send({ status: "error", error: "valores incompletos" });
+  }
+
+  updateProduct = { ...updateProduct, id: idProduct };
+  products[i] = updateProduct;
+  res.send({ status: "success", message: "Producto editado correctamente" });
+});
+
+router.delete("/:id", (req, res) => {
+  const idProduct = +req.params.id;
+  const actualLength = products.length;
+  products = products.filter((product) => product.id != idProduct);
+
+  if (products.length === actualLength) {
+    return res
+      .status(404)
+      .send({ status: "error", error: "Producto no encontrado" });
+  }
+
+  res.send({ status: "success", message: "Producto eliminado correctamente" });
+});
+
+export default router;
